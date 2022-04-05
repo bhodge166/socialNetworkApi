919355df9d710913f14d9a5df4bdcca57964fd42
Brad Hodge <bhodge166@gmail.com>
2022-04-05 15:04:47 -0600

    updated routes and dependencies

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const thoughtRoutes = require("./thoughRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 const reactionRoutes = require("./reactionRoutes");
 
 router.use("/users", userRoutes);
